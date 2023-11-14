9a725cdb5c1b3e1654d811acc53b41760d369eda
Cassio-hue <cassiocvtb2003@gmail.com>
2023-11-14 10:57:01 -0300

    feat: descrevendo regras de negócio do programa

---
+++ hotel.c
@@ -0,0 +1,32 @@
+
+/*
+Hospedes: N?
+Quartos: 10
+Entregador: ??
+Camareira: ??
+
+Descrição do problema:
+Pessoas estão que estão no quarto podem solicitar pedidos. Pessoas quando liberam os quartos, a camareira deve passar e limpar antes de um novo hospede entrar.
+
+HOSPEDE:
+- Pode escolher um quarto
+- Pode realizar um pedido para o Serviço do Hotel
+    - Água
+    - Alimento
+        - Suco de Uva
+        - Barrinha de Cereal
+        - Chocolate
+        - Saguadin
+    - Ir em bora
+
+SERVIÇO DO HOTEL:
+- Alocar um hospede em um quarto
+- Realizar as demandas dos hospedes:
+    - Entregar os pedidos
+    - Desalocar um hospede do quarto e solicitar para a camareira limpar o quarto
+
+CAMAREIRA:
+- Quando um quarto for liberado por um cliente, ela deve ir e limpar o quarto.
+- Após limpar o quarto, deve avisar o serviço do hotel que o quarto está disponível
+
+*/
