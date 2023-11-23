f56767eb81b440437a4a9746e320968b0325d75f
Cassio-hue <cassiocvtb2003@gmail.com>
2023-11-23 11:27:27 -0300

    feat: alocação nos quartos

--- hotel.c
+++ hotel.c
@@ -38,7 +38,8 @@ CAMAREIRA:
 #include <unistd.h>
 #include <semaphore.h>
 
-#define N_HOSPEDES 5
+#define N_QUARTOS 5
+#define N_HOSPEDES 15
 #define N_CAMAREIRAS 2
 #define N_ENTREGADORES 1
 
@@ -50,14 +51,19 @@ void *servico_hotel (void *arg);
 // Variaveis de sincronizacao
 pthread_mutex_t mutex_quartos = PTHREAD_MUTEX_INITIALIZER;
 
+sem_t semaforo_hospedes;
 sem_t semaforo_servico_hotel;
 sem_t semaforo_camareiras;
 sem_t semaforo_entregadores;
 
 
 // Variaveis globais
-int quartos[N_HOSPEDES];
-int pedidos[N_HOSPEDES];
+int quartos[N_QUARTOS];
+int hospede_quarto[N_HOSPEDES];
+int camareira_quarto[N_QUARTOS];
+int pedidos[N_QUARTOS];
+
+int num_hospede_atual;
 
 
 
@@ -69,14 +75,15 @@ int main () {
 
     // Inicializacao dos semaforos
     sem_init(&semaforo_camareiras, 0, 0);
-    // sem_init(&semaforo_servico_hotel, 0, 1);
+    sem_init(&semaforo_hospedes, 0, N_HOSPEDES);
+    sem_init(&semaforo_servico_hotel, 0, 0);
     // sem_init(&semaforo_entregadores, 0, 0);
 
 
 	// Criacao das threads de Hospedes
     pthread_t hospede[N_HOSPEDES];
 
-    for (i = 0; i < N_HOSPEDES; i++) {
+    for (i = 1; i < N_HOSPEDES; i++) {
 	    id = (int *) malloc(sizeof(int));
 	    *id = i;
 	    pthread_create(&hospede[i], NULL, hospedes, (void*) (id));
@@ -101,13 +108,27 @@ int main () {
     }
 
 
-    pthread_join(hospede[0],NULL);
+    // Criacao da thread de Servico do Hotel
+    pthread_t t_servico_hotel;
+    pthread_create(&t_servico_hotel, NULL, servico_hotel, NULL);
+
+
+    pthread_join(camareira[0],NULL);
 	return 0;
+}
+
+
+void linhasVazias(int n) {
+    for (int i = 0; i < n; i++) {
+        printf("\n");
+    }
 }
 
 
 void realizar_pedido(int id_hospede, int num_pedido) {
     int pedido_realizado = 0;
+    // preenche o array de pedidos com o pedido do cara
+    pedidos[id_hospede] = num_pedido;
 
 }
 
@@ -115,24 +136,46 @@ void alocar_quarto(int id_hospede) {
     int num_quarto;
     int quarto_alocado = 0;
 
+    linhasVazias(1);
     printf("Vamos pesquisar se existe algum quarto disponível para o hospede %d. \n", id_hospede);
+    linhasVazias(1);
 
     // Procura um quarto vazio
-    for (num_quarto = 0; num_quarto < N_HOSPEDES; num_quarto++) {
+    for (num_quarto = 0; num_quarto < N_QUARTOS; num_quarto++) {
         if (quartos[num_quarto] == 0) {
             quartos[num_quarto] = id_hospede;
+            hospede_quarto[id_hospede] = num_quarto;
             quarto_alocado = 1;
             break;
         }
     }
 
-
     sleep(2);
     if (quarto_alocado) {
         printf("Hospede %d alocou o quarto %d\n", id_hospede, num_quarto);
+
+        // // Imprime os elementos do array usando um loop for
+        // for (int i = 0; i < N_QUARTOS; i++) {
+        //     printf("%d ", quartos[i]);
+        // }
+        // printf("\n"); // Adiciona uma nova linha no final
+
     } else {
         printf("Hospede %d não conseguiu alocar um quarto\n", id_hospede);
     }
+}
+
+
+int numAleatorio() {
+    int numero_aleatorio;
+    
+    // Define a semente para a função rand() usando o tempo atual
+    srand(time(NULL));
+    
+    // Gera um número aleatório entre 1 e 100
+    numero_aleatorio = rand() % 100 + 1;
+
+    return numero_aleatorio;
 }
 
 
@@ -140,28 +183,73 @@ void * hospedes (void *arg) {
 	
     int id_hospede = *((int *) arg);
 
-	while(1) {
+    // Hospede quer um quarto
+    pthread_mutex_lock(&mutex_quartos);
+        printf("Hospede %d quer reservar um quarto...\n", id_hospede);
+        num_hospede_atual = id_hospede;
+        sem_post(&semaforo_servico_hotel);
+    pthread_mutex_unlock(&mutex_quartos);
+        sem_wait(&semaforo_hospedes);
 
-        // Hospede quer um quarto
+    sleep(5);
+
+    // Hospede libera o quarto e vai embora
+    int acao = numAleatorio();
+    if (acao >= 70) {
+        linhasVazias(2);
+        printf("Hospede %d quer ir embora...\n", id_hospede);
+        linhasVazias(2);
+
         pthread_mutex_lock(&mutex_quartos);
-            printf("Hospede %d quer reservar um quarto...\n", id_hospede);
-            alocar_quarto(id_hospede);
+            int num_quarto = hospede_quarto[id_hospede];
+            quartos[num_quarto] = 0;
+            camareira_quarto[num_quarto] = num_quarto;
+            printf("Hospede %d liberou o quarto %d\n", id_hospede, id_hospede);
+            sem_post(&semaforo_camareiras);
         pthread_mutex_unlock(&mutex_quartos);
+    } 
 
-		sleep(5);
-	} 
+    sleep(3);
+    printf("Hospede %d foi embora...\n", id_hospede);
+    // else {
+    //     linhasVazias(2);
+    //     printf("Hospede %d quer realizar um pedido...\n", id_hospede);
+    //     linhasVazias(2);
+
+    //     pthread_mutex_lock(&mutex_quartos);
+    //         realizar_pedido(id_hospede, acao);
+    //         printf("Hospede %d realizou o pedido %d\n", id_hospede, acao);
+    //     pthread_mutex_unlock(&mutex_quartos);  
+    // }
 }
+
+
+void limpar_quarto() {
+    for (int i = 0; i < N_QUARTOS; i++) {
+        if (quartos[i] == 0) {
+            linhasVazias(2);
+            printf("Camareira está limpando o quarto %d...\n", i);
+            sleep(5);
+            printf("Camareira terminou de limpar o quarto %d...\n", i);
+            linhasVazias(2);
+        }
+        break;  
+    }
+}
+
 
 void * camareiras (void *arg) {
     
     int id_camareira = *((int *) arg);
 
     while(1) {
-        printf("Camareira %d está esperando um quarto para limpar...\n", id_camareira);
         sem_wait(&semaforo_camareiras);
 
-        printf("Camareira %d está limpando o quarto...\n", id_camareira);
-        sleep(5);
+        linhasVazias(2);
+        printf("Camareira %d está esperando um quarto para limpar...\n", id_camareira);
+        linhasVazias(2);
+    
+        limpar_quarto();
     } 
 }
 
@@ -171,7 +259,24 @@ void * entregadores (void *arg) {
 
     while(1) {
         // Entregador quer entregar um pedido
-        printf("Entregador %d quer entregar um pedido...\n", id_entregador);
+        // printf("Entregador %d quer entregar um pedido...\n", id_entregador);
+        sleep(5);
+    } 
+}
+
+void * servico_hotel (void *arg) {
+    
+    while(1) {
+        // Servico do Hotel quer atender um pedido
+        linhasVazias(2);
+            printf("Servico do Hotel está aguardando solicitações dos hospedes!\n");
+            sem_wait(&semaforo_servico_hotel);
+        linhasVazias(2);
+
+        // Servico do Hotel quer alocar um quarto
+        linhasVazias(2);
+            alocar_quarto(num_hospede_atual);
+            sem_post(&semaforo_hospedes);
         sleep(5);
     } 
 }
