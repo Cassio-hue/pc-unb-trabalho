9204e60a63f0c82876b6ce0b25cb50b8cc4d5038
Cassio-hue <cassiocvtb2003@gmail.com>
2023-11-27 22:20:03 -0300

    feat: entregador - deu deadlock, é necessário arrumar

--- hotel.c
+++ hotel.c
@@ -8,20 +8,28 @@
 #define N_QUARTOS 5
 #define N_HOSPEDES 10
 #define N_CAMAREIRAS 2
+#define N_ENTREGADORES 1
 
 void *hospedes (void *arg);
 void *camareiras (void *arg);
+void *entregadores (void *arg);
 
 
 // Variaveis de sincronizacao
 pthread_mutex_t mutex_quartos = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_entregador = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_pedido = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond_entregador = PTHREAD_COND_INITIALIZER;
 sem_t semaforo_camareiras;
+sem_t semaforo_hospedes;
 
 
 // Variaveis globais
 int quartos[N_QUARTOS];
 int camareira_quarto[N_QUARTOS];
 int hospede_quarto[N_HOSPEDES];
+int hospede_pedido[N_HOSPEDES];
+int totalPedidos = 0;
 
 // Funcao principal para executar o programa
 int main () {
@@ -43,8 +51,14 @@ int main () {
         camareira_quarto[i] = -1;
     }
 
+    // Inicializacao do array de pedidos
+    for (int i = 0; i < N_HOSPEDES; i++) {
+        hospede_pedido[i] = -1;
+    }
+
     // Inicializacao dos semaforos
     sem_init(&semaforo_camareiras, 0, 0);
+    sem_init(&semaforo_hospedes, 0, 0);
 
 
     // Criacao das threads de Camareiras
@@ -66,23 +80,23 @@ int main () {
 
 
     // Criacao da thread de Entregador
-    // pthread_t entregador[N_ENTREGADORES];
-    // for (i = 0; i < N_ENTREGADORES; i++) {
-    //     id = (int *) malloc(sizeof(int));
-    //     *id = i;
-    //     pthread_create(&entregador[i], NULL, entregadores, (void*) (id));
-    // }
+    pthread_t entregador[N_ENTREGADORES];
+    for (i = 0; i < N_ENTREGADORES; i++) {
+        id = (int *) malloc(sizeof(int));
+        *id = i;
+        pthread_create(&entregador[i], NULL, entregadores, (void*) (id));
+    }
 
     pthread_join(camareira[0],NULL);
 	return 0;
 }
 
-int numAleatorio() {
+int numAleatorio(int range) {
     // Define a semente para a função rand() usando o tempo atual
     srand(time(NULL));
     
-    // Gera um número aleatório entre 1 e 100
-    return rand() % 100 + 1;
+    // Gera um número aleatório entre 0 e range
+    return rand() % range + 1;
 }
 
 
@@ -104,7 +118,8 @@ void * hospedes (void *arg) {
         pthread_mutex_unlock(&mutex_quartos);
         }
 
-        int quer_sair = numAleatorio();
+        int quer_sair = numAleatorio(100);
+        int pedido = 0;
         
         if (hospede_quarto[id_hospede] != -1 && quer_sair >= 70) {
             printf("HOSPEDE %d: quer sair do quarto %d\n", id_hospede, hospede_quarto[id_hospede]);
@@ -117,8 +132,54 @@ void * hospedes (void *arg) {
             pthread_mutex_unlock(&mutex_quartos);
         } 
         else if (hospede_quarto[id_hospede] != -1 && quer_sair < 70) {
-            printf("HOSPEDE %d: está dormindo...\n", id_hospede);
-            sleep(5);
+
+            pedido = numAleatorio(3);
+            int num_quarto = hospede_quarto[id_hospede];
+
+            if(pedido == 0) {
+
+                printf("HOSPEDE %d: está pedindo comida no quarto %d\n", id_hospede, num_quarto);
+
+                pthread_mutex_lock(&mutex_pedido);
+                    totalPedidos++;
+                    hospede_pedido[id_hospede] = 0;
+                pthread_mutex_lock(&mutex_pedido);
+                pthread_cond_signal(&cond_entregador);
+
+                sem_wait(&semaforo_hospedes);
+                sleep(2);
+
+            }
+
+            if(pedido == 1) {
+                    
+                printf("HOSPEDE %d: está pedindo bebida no quarto %d\n", id_hospede, num_quarto);
+                sleep(2);
+
+                pthread_mutex_lock(&mutex_pedido);
+                    totalPedidos++;
+                    hospede_pedido[id_hospede] = 1;
+                pthread_mutex_lock(&mutex_pedido);
+                pthread_cond_signal(&cond_entregador);
+
+                sem_wait(&semaforo_hospedes);
+
+            }
+
+            if(pedido == 2) {
+                        
+                printf("HOSPEDE %d: está pedindo toalhas no quarto %d\n", id_hospede, num_quarto);
+                sleep(2);
+
+                pthread_mutex_lock(&mutex_pedido);
+                    totalPedidos++;
+                    hospede_pedido[id_hospede] = 2;
+                pthread_mutex_lock(&mutex_pedido);
+                pthread_cond_signal(&cond_entregador);
+
+                sem_wait(&semaforo_hospedes);
+
+            }
         } else {
 
             sleep(1);
@@ -149,5 +210,47 @@ void * camareiras (void *arg) {
                 }
             }
         pthread_mutex_unlock(&mutex_quartos);
+    }
+}
+
+void * entregadores (void *arg) {
+    int id_entregador = *((int *) arg);
+
+    while (1) {
+        pthread_mutex_lock(&mutex_entregador);
+            while (totalPedidos == 0) {
+                pthread_cond_wait(&cond_entregador, &mutex_entregador);
+            }
+
+            pthread_mutex_lock(&mutex_pedido);
+                totalPedidos--;
+            pthread_mutex_unlock(&mutex_pedido);
+
+            for (int i = 0; i < N_HOSPEDES; i++) {
+                if (hospede_pedido[i] != -1) {
+
+                    if(hospede_pedido[i] == 0) {
+                        printf("ENTREGADOR %d: está entregando a comida para o hospede %d\n", id_entregador, i);
+                    }
+
+                    if(hospede_pedido[i] == 1) {
+                        printf("ENTREGADOR %d: está entregando a bebida para o hospede %d\n", id_entregador, i);
+                    }
+
+                    if(hospede_pedido[i] == 2) {
+                        printf("ENTREGADOR %d: está entregando as toalhas para o hospede %d\n", id_entregador, i);
+                    }
+
+                    pthread_mutex_lock(&mutex_pedido);
+                        hospede_pedido[i] = -1;
+                    pthread_mutex_unlock(&mutex_pedido);
+                    sem_post(&semaforo_hospedes);
+                    break;
+                }
+            }
+
+        pthread_mutex_unlock(&mutex_entregador);
+        sleep(2);
+        
     }
 }
